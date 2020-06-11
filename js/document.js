4ca2a6c713a24d2e976ee9216296bee4f723ba16
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-06-11 17:27:22 +0700

    upgrade

--- js/document.js
+++ js/document.js
@@ -1,12 +1,162 @@
 $(document).ready(function () {
-    init();
+    loginBody();
+    var iUri;
+    var iWsServers;
+    var iUser;
+    var iPassword;
+    var iProxy;
     $(document).on("click", "#callBtn", function () {
         var addr = $(`input[id="callAddress"]`).val();
         call(addr);
     });
-    $(document).on("deny", "#callBtn", function () {
+    $(document).on("click", "#deny", function () {
         $('#income').empty();
         $('#cameraScreen').empty();
     });
+    $(document).on("click", "#exit", function () {
+        loginBody();
+    });
+    $(document).on("click", "#saveconfig", function () {
+        iUser = $(`input[id="authoruser"]`).val();
+        iPassword = $(`input[id="pass"]`).val();
+        $(`input[id="rgpass"]`).val(iPassword);
+        $(`input[id="rguser"]`).val(iUser);
+        $(`input[id="tab-1"]`).prop('checked', true);
+
+    });
+    $(document).on("click", "#addBtn", function () {
+        
+        var name = $(`input[id="name"]`).val();
+        var addr = $(`input[id="callAddress"]`).val();
+        if (!addr.includes(domain))
+        {
+            addr = addr + domain;
+        }
+
+        $('#contactTable').append(`
+                        <tr>
+                            <td width="20$" style="vertical-align: middle;">${name}</td>
+                            <td width="40%" style="vertical-align: middle;">${addr}</td>
+                            <td width="40%">
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success" onclick="call('${addr}')">Call</button>
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success">Edit</button>
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success">Delete</button>
+                            </td>
+                        </tr>
+            `
+        )
+
+    });
+
+    
+    $(document).on("click", "#registerBtn", function () {
+        iUri = $(`input[id="uri"]`).val();
+        iWsServers = $(`input[id="wss"]`).val();
+        iUser = $(`input[id="authoruser"]`).val();
+        iPassword = $(`input[id="pass"]`).val();
+        iProxy = $(`input[id="proxy"]`).val();
+        var check;
+        if ($('#icheck').is(":checked")) {
+            setAllCookie(iUri,iWsServers,iPassword,iProxy,iUser);
+            console.log('setcookie');
+        }
+        document.cookie = "user=John; path=/index; expires=Tue, 19 Jan 2038 03:14:07 GMT"
+        var a  = getCookie("user");
+        console.log(a);
+        init(iUri,iWsServers,iPassword,iProxy,iUser);
+    });
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + value + ((expires==null) ? "" : ";expires=" + expires)
+
+      }
+      function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+      }
+    function setAllCookie(iUri,iWsServers,iPassword,iProxy,iUser)
+    {
+        setCookie('URI',iUri,3);
+        setCookie('WSS',iWsServers,3);
+        setCookie('PASS',iPassword,3);
+        setCookie('PROXY',iProxy,3);
+        setCookie('USER',iUser,3);
+    }
     
 })  
+
+function loginBody()
+{
+    $('#body').empty();
+    $('#body').append(`
+    <audio id="remoteAudio"></audio>
+	<audio id="loaclAudio" muted="muted"></audio>
+	<div class="login-wrap">
+		<div class="login-html">
+			<input id="tab-1" type="radio" name="tab" class="sign-in" checked='true'><label for="tab-1"
+				class="tab">Register</label>
+			<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Configure</label>
+			<div class="login-form">
+				<div class="sign-in-htm">
+					<div class="group">
+						<label for="rguser" class="label">Username</label>
+						<input id="rguser" type="text" class="input" value="203">
+					</div>
+					<div class="group">
+						<label for="rgpass" class="label">Password</label>
+						<input id="rgpass" type="password" class="input" data-type="password" value="TEL4VN.COM@2020">
+					</div>
+					<div class="group">
+						<input id="icheck" type="checkbox" class="check" checked>
+						<label for="icheck"><span class="icon"></span> Keep me register</label>
+
+					</div>
+					<div class="group">
+						<input type="submit" id='registerBtn' class="button" value="Register">
+					</div>
+				</div>
+				<div class="sign-up-htm">
+					<div class="group">
+						<label for="uri" class="label">SIP URI</label>
+						<input id="uri" type="text" class="input" value="203@tel4vn-vn01.tel4vn.com">
+					</div>
+					<div class="group">
+						<label for="pass" class="label">Password</label>
+						<input id="pass" type="password" class="input" data-type="password" value="TEL4VN.COM@2020">
+					</div>
+					<div class="group">
+						<label for="wss" class="label">Websocket URI</label>
+						<input id="wss" class="input" value="wss://sbc03.tel4vn.com:7444">
+					</div>
+					<div class="group">
+						<label for="proxy" class="label">Proxy</label>
+						<input id="proxy"class="input" value="sbc03.tel4vn.com">
+					</div>
+					<div class="group">
+						<label for="authoruser" class="label">Authorization User</label>
+						<input id="authoruser" type="text" class="input" value="203">
+					</div>
+					<div class="group">
+						<input id='saveconfig' type="submit" class="button" value="Save">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>`);
+    
+	
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 var isRegisted = false;
+var domain = "@tel4vn-vn01.tel4vn.com";
 var simple = null;
-function init() {
-    
-    var iUri = $(`input[id="uriAddr"]`).val();
-    var iWsServers = $(`input[id="wsServers"]`).val();
-    var iPort = $(`input[id="port"]`).val();
-    var iUSer = $(`input[id="authorUser"]`).val();
-    var iPassword = $(`input[id="password"]`).val();
-    var iProxy = $(`input[id="proxy"]`).val();
-    if (!iWsServers.includes("wss://"))
-    {
-        iWsServers = "wss://"+iWsServers;
+var user;
+var uri;
+var onring = false;
+function init(iUri, iWsServers, iPassword, iProxy, iUSer) {
+    user = iUSer;
+    uri = iUri;
+    if (!iWsServers.includes("wss://")) {
+        iWsServers = "wss://" + iWsServers;
     }
     var options = {
         media: {
@@ -24,121 +22,239 @@ function init() {
         ua:
         {
             uri: iUri,
-            wsServers: iWsServers+":" + iPort,
+            wsServers: iWsServers,
             password: iPassword,
-            authorizationUser:iUSer,
-            proxy : iProxy
+            authorizationUser: iUSer,
+            proxy: iProxy
         }
     };
     simple = new SIP.Web.Simple(options);
+    tata.info('Waiting', 'Waiting for registration', { position: 'mt', animate: 'slide', duration: 3000 })
     addEvent(simple);
 }
-function addComponent()
-{
+function addComponent() {
     $("#income").append(`
-        <div class="col-md-2">
+                        <div class="col-md-6">
                             <button id='accept'  onclick="answer()"
                                 class="btn btn-outline-info btn-rounded btn-block z-depth-0">Answer</button>
                         </div>
-                        <div class="col-md-2">
+                        <div class="col-md-6">
                             <button id='deny' onclick="reject()"
                                 class="btn btn-outline-info btn-rounded btn-block z-depth-0">Deny</button>
-                        </div>
-                        <div class="col-md-2">
-                            <button id='hangup' onclick="hangup()"
-                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Hangup</button>
-                        </div>
-                        <div class="col-md-2">
-                            <button id='mute' onclick="mute()"
-                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Mute</button>
-                        </div>
-                        <div class="col-md-2">
-                            <button id='unmute' onclick="unmute()"
-                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Unmute</button>
-                        </div>
-                        <div class="col-md-2">
-                            <button id='hold'
-                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Hold</button>
                         </div>`
-                        );
-        $('#cameraScreen').append(`
+    );
+    $('#cameraScreen').append(`
         <div class="col-xl-12 col-md-12 mb-4">
                         <div class="card border-3 shadow" 
                             style="display: flex;align-content: center;justify-items: center;align-items: center;margin: auto;border-radius:1rem">
-                            <img src="loading.gif" height="640px" width="480px" class="card-img-top">
+                            <img src="loading.gif" height="320px" width="240px" class="card-img-top">
                         </div>
                     </div>
-        `);      
-                      
+        `);
+
 }
 
-function addEvent(connection)
-{
-    connection.on('registered',function(event) {
-    isRegisted = true;
-    tata.success('Success', 'Register success', { position: 'ml', animate: 'slide', duration: 1500 })
+function addEvent(connection) {
+    connection.on('registered', function (event) {
+        isRegisted = true;
+        indexBody(user, uri);
+        tata.success('Success', 'Register success', { position: 'ml', animate: 'slide', duration: 1500 })
     })
-    connection.on('unregistered',function(event) {
+    connection.on('unregistered', function (event) {
         console.log('EVENT : UNREG')
-        if (isRegisted==false) {tata.error('Failed', 'Incorrect infomation', { position: 'ml', animate: 'slide', duration: 1500 })}
-        else {tata.error('Failed', 'Other reasons', { position: 'ml', animate: 'slide', duration: 1500 })};
+        if (isRegisted == false) { tata.error('Failed', 'Incorrect infomation', { position: 'ml', animate: 'slide', duration: 1500 }) }
+        else { tata.error('Failed', 'Other reasons', { position: 'ml', animate: 'slide', duration: 1500 }) };
     })
-    connection.on('ringing',function(event) {
+    connection.on('ringing', function (event) {
+        if (onring == false) {
+            addComponent();
+        }
+        onring = true;
         console.log('EVENT : RINGING')
-        addComponent();
-          
+
     })
-    connection.on('connecting',function(event) {
+    connection.on('connecting', function (event) {
         console.log('EVENT : CONNECTING')
     })
-    connection.on('connected',function(event) {
-        console.log('EVENT : CONNECTING')
+    connection.on('rejected', function (response, cause) {
+        console.log('EVENT : REJECTED');
     })
-    connection.on('ended',function(event) {
+    connection.on('accepted', function (data) {
+        console.log('EVENT : accepted')
+    })
+
+    connection.on('connected', function (event) {
+        console.log('EVENT : Connected')
+        incall();
+    })
+    connection.on('ended', function (event) {
         console.log('EVENT : END')
         $('#income').empty();
         $('#cameraScreen').empty();
+        onring = false;
 
     })
-    connection.on('mute',function(event) {
+    connection.on('mute', function (event) {
         console.log('EVENT : MUTE')
     })
-    connection.on('unmute',function(event) {
+    connection.on('unmute', function (event) {
         console.log('EVENT : UNMUTE')
     })
 }
-
-
-function call(addr)
-{
-    if (!isRegisted)
-    {
+function call(addr) {
+    if (!isRegisted) {
         tata.error('Failed', 'Please register', { position: 'ml', animate: 'slide', duration: 1500 })
     }
+    if (addr.includes(domain)) { simple.call(addr) }
+    else {
+        simple.call(addr + domain);
+    }
     addComponent();
-    simple.call(addr);
+    incall();
+    console.log(addr);
 }
-function answer()
-{
+function answer() {
     simple.answer();
 }
-function reject()
-{
+function reject() {
+    console.log('reject');
     simple.reject();
 }
-function mute()
-{
+function mute() {
     simple.mute();
 }
-function unmute()
-{
+function unmute() {
     simple.unmute();
 }
-function hangup()
-{
+function hangup() {
     simple.hangup();
 }
-function hold()
-{
+function hold() {
     simple.hold();
+}
+function indexBody(user, uri) {
+    $('#body').empty();
+    $('#body').append(`
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Tel4vn</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">${user}<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">${uri}</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id='exit'>Exit</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <audio id="remoteAudio"></audio>
+<audio id="loaclAudio" muted="muted"></audio>
+<div id='main' class="row" style="margin: 10px 50px;">
+<div class="col-xl-6 col-md-6 mb-6">
+    <div class="row">
+
+        <div class="card border-3 shadow" style="margin-right: 75px;
+border-radius: 1rem;
+width: -webkit-fill-available;">
+            <div style="padding: 10px 50px;">
+                <table class="table">
+                    <thead class="black white-text">
+                        <tr>
+                            <th width="20%" scope="col">Name</th>
+                            <th width="40%" scope="col">Phone</th>
+                            <th width="40%" scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id='contactTable'>
+                        <tr>
+                            <td width="20$" style="vertical-align: middle;">202</td>
+                            <td width="40%" style="vertical-align: middle;">202@tel4vn-vn01.tel4vn.com</td>
+                            <td width="40%">
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success" onclick="call('202')">Call</button>
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success">Edit</button>
+                                <button name="203" style="padding: 3px 15px;" type="button"
+                                    class="btn btn-outline-success">Delete</button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Call -->
+<div class="col-xl-6 col-md-6 mb-6">
+    <div class="card border-3 shadow" style="border-radius:1rem">
+
+        <h2 style="margin: 10px;text-align: center;">Make a call</h2>
+        <div style="padding: 10px 50px;">
+            <div class="row">
+                <div class="col-md-4" style="display: flex;">
+                    <input id='name' type="text" placeholder="Name" class="form-control" value="202">
+                </div>
+                <div class="col-md-6" style="display: flex;">
+                    <input id='callAddress' type="text" placeholder="Address to call" class="form-control"
+                        value="202@tel4vn-vn01.tel4vn.com">
+                </div>
+                <div class="col-md-1">
+                    <button id='callBtn'
+                        class="btn btn-outline-info btn-rounded btn-block z-depth-0">Call</button>
+                </div>
+                <div class="col-md-1">
+                    <button id='addBtn'
+                        class="btn btn-outline-info btn-rounded btn-block z-depth-0">Add</button>
+                </div>
+            </div>
+            <!--  -->
+
+            <!-- Camera Screen -->
+            <div id='cameraScreen' class="row">
+
+            </div>
+            <div id='income' class="row">
+
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div id="domain">
+<p>
+    <bold>Contact domain : @tel4vn-vn01.tel4vn.com</bold>
+</p>
+</div>
+    `);
+}
+
+function incall()
+{
+    $("#income").empty();
+    $("#income").append(`
+                        <div class="col-md-3">
+                            <button id='hangup' onclick="hangup()"
+                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Hangup</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button id='mute' onclick="mute()"
+                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Mute</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button id='unmute' onclick="unmute()"
+                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Unmute</button>
+                        </div>
+                        <div class="col-md-3">
+                            <button id='hold'
+                                class="btn btn-outline-info btn-rounded btn-block z-depth-0">Hold</button>
+                        </div>`
+    );
 }
